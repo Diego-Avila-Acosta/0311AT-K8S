7a430ce053edeca2569f6a487eb2539823f1df10
Diego <diegoavila@DESKTOP-JRF231L>
2025-05-02 20:29:16 -0300

    Se añadio la verificación de dependencias

--- script.sh
+++ script.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+verify_dependecies(){
+	if ! which $1 > /dev/null; then
+		echo "Error: Se requiere '$1' pero no está instalado o no está en el PATH." >&2
+		exit 1
+	fi
+}
+
+verify_dependecies git
+verify_dependecies docker
+verify_dependecies minikube
+verify_dependecies kubectl
