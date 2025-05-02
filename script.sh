15d1eb0a98b9805f0d520ac8c981f72f2c9ec86e
Diego Avila <aviladiego2009@gmail.com>
2025-05-02 20:57:47 -0300

    Se añadió comandos para clonar el repositorio

--- script.sh
+++ script.sh
@@ -3,6 +3,8 @@
 set -e
 set -o pipefail
 
+WEBSITE_URL="https://github.com/Diego-Avila-Acosta/static-website"
+
 verify_dependecies(){
 	if ! which $1 > /dev/null; then
 		echo "Error: Se requiere '$1' pero no está instalado o no está en el PATH." >&2
@@ -14,3 +16,13 @@ verify_dependecies git
 verify_dependecies docker
 verify_dependecies minikube
 verify_dependecies kubectl
+
+
+if [ ! -d "./static-website/.git" > /dev/null ]; then
+	echo "Repositorio no encontrado. Clonando..."
+	git clone $WEBSITE_URL
+else
+	echo "Repositorio ya existente. Actualiznado"
+	cd "static-website"
+	git pull
+fi
