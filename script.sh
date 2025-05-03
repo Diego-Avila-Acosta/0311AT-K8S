f748ecbfc64deba45f37b46438f0020b30579f75
Diego Avila <aviladiego2009@gmail.com>
2025-05-02 21:19:46 -0300

    Se añadió mas variables al script

--- script.sh
+++ script.sh
@@ -3,12 +3,16 @@
 set -e
 set -o pipefail
 
+SCRIPT_DIR="$(dirname "$0")"
+PROJECT_URL="${PWD}/static-website"
+
 #URL de la página por defecto
 WEBSITE_URL="https://github.com/Diego-Avila-Acosta/static-website"
 
 #Si el usuario provee su propio repositorio website
 WEBSITE_URL="${1:-$WEBSITE_URL}"
 
+#Funcion para verificar dependencias
 verify_dependecies(){
 	if ! which $1 > /dev/null; then
 		echo "Error: Se requiere '$1' pero no está instalado o no está en el PATH." >&2
@@ -22,11 +26,10 @@ verify_dependecies minikube
 verify_dependecies kubectl
 
 
-if [ ! -d "./static-website/.git" > /dev/null ]; then
+if [ ! -d "${PROJECT_URL}/.git" > /dev/null ]; then
 	echo "Repositorio no encontrado. Clonando repositorio de $WEBSITE_URL"
 	git clone $WEBSITE_URL
 else
 	echo "Repositorio ya existente. Actualizando"
-	cd "static-website"
-	git pull
+	cd "static-website" && git pull && cd ..
 fi
